aea4646494d784b550cc4f31874d5c246e26b315
Shehan-Amarasinghe <Shehan.nivantha124@gmail.com>
2024-05-09 19:22:59 +0530

    Using props and useState for both App.tsx and Todo.tsx files.

--- src/Components/Todo.tsx
+++ src/Components/Todo.tsx
@@ -1,5 +1,6 @@
 import CheckIcon from '@mui/icons-material/Check';
 import DeleteIcon from '@mui/icons-material/Delete';
+<<<<<<< HEAD
 import { TodoType } from '../types';
 
 interface TodoProps{
@@ -12,11 +13,27 @@ const Todo = ({todos, deleteTodo}:TodoProps) => {
         <li className="p-2 my-2 uppercase bg-teal-300 rounded-md shadow-sm">
             <div className='flex items-center justify-between'>
                 <p>{todos.text} </p>
+=======
+
+interface TodoProps{
+    todos: string;
+}
+
+const Todo = ({todos}: TodoProps) => {
+    return (
+        <li className="p-2 my-2 uppercase bg-teal-300 rounded-md shadow-sm">
+            <div className='flex items-center justify-between'>
+                <p>{todos} </p>
+>>>>>>> e696781 (Using props and useState for both App.tsx and Todo.tsx files.)
                     <div className='flex gap-3'>
                         <button>
                             <CheckIcon/>
                         </button>
+<<<<<<< HEAD
                         <button onClick={()=> deleteTodo(todos.id)}>
+=======
+                        <button>
+>>>>>>> e696781 (Using props and useState for both App.tsx and Todo.tsx files.)
                             <DeleteIcon/>
                         </button>
                     </div>  

cd38488b77ff3891ac3136792d195558640a3e92
Shehan-Amarasinghe <Shehan.nivantha124@gmail.com>
2024-05-13 20:20:58 +0530

    Added several jsx files and create todo function

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,27 @@
 import Form from "./Components/Form";
 import { useEffect, useState } from "react";
 import Todo from "./Components/Todo";
-import { QuerySnapshot, collection, deleteDoc, doc, onSnapshot, query } from "firebase/firestore";
+import { QuerySnapshot, addDoc, collection, deleteDoc, doc, onSnapshot, query } from "firebase/firestore";
 import { db } from "./Components/Firebase";
 
 const App = () => {
   
-  const [todo, setTodo] = useState([""]);
+  const [todo, setTodo] = useState([]);
+  const [input, setInput] = useState('')
 
   // Create Todo
+  const createTodo = async (e) => {
+    e.preventDefault()
+    if (input === ''){
+      alert("Please enter some task")
+      return
+    }
+    await addDoc (collection (db, "todos"), {
+      text: input, 
+      completed:false
+    })
+    setInput('')
+  }
   
   // Read Todo
   useEffect(()=>{
@@ -33,10 +46,10 @@ const App = () => {
   };
   
   return (
-  <div className="w-screen h-screen p-4 bg-teal-300">
+  <div className="w-screen h-screen p-4 overflow-y-scroll bg-teal-300">
     <div className="bg-slate-100 max-w-[500px] w-full m-auto rounded-md shadow-xl p-4">
         <h1 className="p-2 text-3xl font-bold text-center text-gray-800">TO-DO LIST</h1>
-       <Form/>
+       <Form createTodo={createTodo} input={input} setInput={setInput}/>
        <ul>
         {todo.map((todos, index) => (
           <Todo key={index} todos={todos} deleteTodo={deleteTodo}/>
--- src/Components/Form.tsx
+++ src/Components/Form.tsx
@@ -1,9 +1,16 @@
 import AddCircleIcon from '@mui/icons-material/AddCircle';
 
-const Form = () => {
+interface FormPropTypes{
+    input: string
+    setInput: (input:string)=>void
+}
+
+const Form = ({createTodo, input, setInput}:FormPropTypes) => {
     return (
-        <form className='flex items-center justify-between p-4 bg-teal-300 rounded-lg'>
-            <input type="text" className='w-full text-xl rounded-lg p-[3px]'/>
+        <form onSubmit={createTodo} className='flex items-center justify-between p-4 bg-teal-300 rounded-lg'>
+            <input 
+            onChange={(e)=> setInput(e.target.value)} value={input}
+            type="text" className='w-full text-xl rounded-lg p-[3px]'/>
             <button className='ml-2 text-gray-800 rounded-lg'>
                 <AddCircleIcon/>
             </button>
